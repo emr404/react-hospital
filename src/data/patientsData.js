335310f23d327b956393fe0ccaf68a814eb783d7
Edouard Roufay <roufaym@gmail.com>
2023-10-23 23:44:46 +0100

    Added data

---
+++ src/data/patientsData.js
@@ -0,0 +1,39 @@
+const patients = [
+    {
+        id: 1,
+        hospitalNumber: '6533794',
+        name: 'Nick Smith',
+        gender: 'Male',
+        dateOfBirth: '1965-07-21',
+    },
+    {
+        id: 2,
+        hospitalNumber: '2571803',
+        name: 'Roy Patel',
+        gender: 'Male',
+        dateOfBirth: '1972-02-11',
+    },
+    {
+        id: 3,
+        hospitalNumber: '8455435',
+        name: 'Nia Odumbe',
+        gender: 'Female',
+        dateOfBirth: '1986-11-27',
+    },
+    {
+        id: 4,
+        hospitalNumber: '2494668',
+        name: 'Jennifer Richardson',
+        gender: 'Female',
+        dateOfBirth: '1955-06-05',
+    },
+    {
+        id: 5,
+        hospitalNumber: '1245763',
+        name: 'Ahmed Khan',
+        gender: 'Male',
+        dateOfBirth: '1996-04-08',
+    },
+];
+
+export default patients;
