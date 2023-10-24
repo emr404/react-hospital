e2f486a648f8070a7aa6ae58e1fb9ae40b83e840
Edouard Roufay <roufaym@gmail.com>
2023-10-24 17:24:35 +0100

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
+    content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/views/**/*.{js,ts,jsx,tsx}'],
+    purge: {
+        options: {
+            safelist: [
+                'bg-blue-500',
+                'hover:bg-blue-700',
+                'bg-red-500',
+                'hover:bg-red-700',
+                'bg-green-500',
+                'hover:bg-green-700',
+                'bg-gray-500',
+                'hover:bg-gray-700',
+                'bg-cream-500',
+                'cursor-pointer',
+            ],
+        },
+    },
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
 
